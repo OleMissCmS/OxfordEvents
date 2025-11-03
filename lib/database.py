21518c1f1004197494a841bb51657ff44f7032af
Chad Stefaniak <chads@ACCSCH.OLEMISS.EDU>
2025-11-02 22:20:08 -0600

    Add missing time import to database module

--- lib/database.py
+++ lib/database.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from datetime import datetime
 import os
+import time
 
 Base = declarative_base()
 
